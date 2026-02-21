c351c0244ca0b8e0f6ef8c6f1f7c27e4c6e47eee
Lingaeswari Kathirvel <ambikavathy658@gmail.com>
2026-02-21 18:10:40 +0530

    Final UI

--- test_tools.py
+++ test_tools.py
@@ -7,10 +7,11 @@ send_email(
     body="This is a real Gmail API test email."
 )
 
+# Test Calendar
 create_calendar_invite(
-    title="Milestone 4 Test Meeting",
+    summary="Milestone 4 Test Meeting",
     description="Testing real Google Calendar API",
-    start_time="2026-01-21T10:00:00",
-    end_time="2026-01-21T10:30:00"
+    start_time="2026-02-19T10:00:00",
+    end_time="2026-02-19T10:30:00",
+    attendees=["lingaeswarikathir@gmail.com"]
 )
-
